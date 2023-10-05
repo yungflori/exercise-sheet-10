f7536319f0418c900083179ec7bbc168bcd7e80c
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-10-05 17:37:36 +0200

    remove teaching exercise

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet10/teacher/BuggyHamsterGameApp.java
+++
@@ -1,9 +0,0 @@
-package de.unistuttgart.iste.sqa.pse.sheet10.teacher;
-
-public class BuggyHamsterGameApp {
-
-	public static void main(String[] args) {
-		final BuggyHamsterGame game = new BuggyHamsterGame();
-		game.doRun();
-	}
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,4 @@
 open module exercise {
-	exports de.unistuttgart.iste.sqa.pse.sheet10.teacher;
 
 	requires de.hamstersimulator.objectsfirst.main;
 }
