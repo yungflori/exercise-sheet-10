b4f60a9921d39f92e05a033418b5ae1ed0488c40
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2023-11-13 12:17:50 +0100

    delete clean code for exercise, that got moved away.

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet10/presence/cleancode/Colour.java
+++
@@ -1,8 +0,0 @@
-package de.unistuttgart.iste.sqa.pse.sheet10.presence.cleancode;
-
-public enum Colour {
-	RED,
-	BLUE,
-	YELLOW,
-	GREEN
-}
--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet10/presence/cleancode/Zone.java
+++
@@ -1,7 +0,0 @@
-package de.unistuttgart.iste.sqa.pse.sheet10.presence.cleancode;
-
-public enum Zone {
-	START,
-	FIELD,
-	GOAL
-}
